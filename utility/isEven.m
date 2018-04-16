49b86e4634845a3238ce3d6b68578272e8afd428
frei <frei@AMT109.uni-duisburg.de>
2018-04-16 13:55:32 +0200

    Initial Commit

---
+++ utility/isEven.m
@@ -0,0 +1,12 @@
+function tf = isEven(number)
+%ISEVEN Determine, whether elements of an array are even.
+
+% Validate input.
+validateattributes( ...
+    number, ...
+    {'numeric'}, ...
+    {'real','finite','nonnan','nonsparse','nonempty','integer'});
+
+tf = logical(mod(number,2));
+end
+
