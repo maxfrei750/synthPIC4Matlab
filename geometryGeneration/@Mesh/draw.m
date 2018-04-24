3616d7ce05731cb57dba6b1856b2f65530e24038
Max Frei <max.frei@uni-due.de>
2018-04-24 11:37:27 +0200

    Added new parameter: coloringMode

--- geometryGeneration/@Mesh/draw.m
+++ geometryGeneration/@Mesh/draw.m
@@ -1,18 +1,49 @@
-function hPatch = draw(obj)
-% Errorhandling for the color variable is performed by the
-% 'patch' class.
+function hPatch = draw(obj,coloringMode)
 
-% Draw the geometry.
+%% Validate inputs
+expectedColoringModes = {
+    'texture'
+    'objectID'
+    };
+
+defaultColoringMode = 'texture';
+
+% Validation functions
+isValidObj = @(x) isa(obj,'Mesh');
+
+isValidColoringMode = @(x) any(validatestring(x,expectedColoringModes));
+
+p = inputParser;
+
+p.addRequired('obj',isValidObj);
+p.addOptional('coloringMode',defaultColoringMode,isValidColoringMode);
+
+p.parse(obj,coloringMode);
+
+coloringMode = p.Results.coloringMode;
+obj = p.Results.obj;
+
+
+%% Draw the geometry.
 hPatch = drawMesh(obj);
 
-set(hPatch, ...
-    fphong, ...
-    'FaceVertexCData',obj.texture);
+switch lower(coloringMode)
+    case 'texture'
+        set(hPatch, ...
+            fphong, ...
+            'FaceVertexCData',obj.texture);
+    case 'objectid'
+        colormap('parula');
+        hPatch.LineStyle = 'none';
+        hPatch.FaceColor = 'flat';
+        hPatch.FaceVertexCData = obj.facesObjectIDs;
+end
+
 
 view(3)
 daspect([1 1 1])
 
-% If no outputargument was requested, then clear hPatch.
+%% If no outputargument was requested, then clear hPatch.
 if nargout == 0
     clear hPatch
 end
