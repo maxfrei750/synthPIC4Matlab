0d050829e48c125323a93abe32aeb32873b4d4fc
Max Frei <max.frei@uni-due.de>
2018-04-25 17:45:38 +0200

    Initial commit.

--- utility/imshowtiles.m
+++ utility/imshowtiles.m
@@ -10,6 +10,8 @@ hold on
 nTiles_x = floor(imageWidth/tileSize);
 nTiles_y = floor(imageHeight/tileSize);
 
+nTiles = nTiles_x*nTiles_y;
+
 for iTile_x = 1:nTiles_x-1
     plot(ones(2,1)*iTile_x*tileSize,[0 imageHeight],'w:','LineWidth',0.5);
 end
@@ -18,11 +20,13 @@ for iTile_y = 1:nTiles_y-1
     plot([0 imageWidth],ones(2,1)*iTile_y*tileSize,'w:','LineWidth',0.5);
 end
 
-for iTile_x = 1:nTiles_x
-    for iTile_y = 1:nTiles_y
-        tileString = sprintf('%d,%d',iTile_x,iTile_y);
-        text((iTile_x-0.5)*tileSize,(iTile_y-0.5)*tileSize,tileString,'HorizontalAlignment','center','FontSize',8,'color','w');
-    end
+for iTile = 1:nTiles
+    % Calculate current tile indices in x- andy y- direction.
+    iTile_x = mod(iTile-1,nTiles_x)+1;
+    iTile_y = floor((iTile-1)/nTiles_x)+1;
+    
+    tileString = sprintf('%d',iTile);
+    text((iTile_x-0.5)*tileSize,(iTile_y-0.5)*tileSize,tileString,'HorizontalAlignment','center','FontSize',8,'color','w');
 end
 
 hold off
