997143a6e4b81ea44e487d5c423d271dda64e21e
Max Frei <max.frei@uni-due.de>
2018-05-02 11:34:44 +0200

    Prepared use of copyable objects as aggloemrates.

--- geometryGeneration/@Agglomerate/addchild.m
+++ geometryGeneration/@Agglomerate/addchild.m
@@ -1,8 +1,10 @@
-function obj_A = addchild(obj_A,obj_B)
+function obj_C = addchild(obj_A,obj_B)
 %ADDCHILD Summary of this function goes here
 %   Detailed explanation goes here
 
-obj_A.childList = [obj_A.childList obj_B.childList];
-obj_A.nChildren = obj_A.nChildren+obj_B.nChildren;
+obj_C = Agglomerate;
+
+obj_C.childList = [obj_A.childList obj_B.childList];
+obj_C.nChildren = obj_A.nChildren+obj_B.nChildren;
 end
 
