ada8fd606eb633ab763effbfa4279da2d8988bbe
Max Frei <max.frei@uni-due.de>
2018-05-08 09:57:52 +0200

    No longer create a new object which is returned.

--- geometryGeneration/@Agglomerate/addchild.m
+++ geometryGeneration/@Agglomerate/addchild.m
@@ -2,7 +2,7 @@ function obj_C = addchild(obj_A,obj_B)
 %ADDCHILD Summary of this function goes here
 %   Detailed explanation goes here
 
-obj_C = Agglomerate;
+obj_C = obj_A;
 
 obj_C.childList = [obj_A.childList obj_B.childList];
 obj_C.nChildren = obj_A.nChildren+obj_B.nChildren;
