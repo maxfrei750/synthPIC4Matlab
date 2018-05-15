dcf441b6be7a850d24ec8a033a8a2af056ff50e2
Max Frei <max.frei@uni-due.de>
2018-05-15 12:24:24 +0200

    Initial commit.

---
+++ geometryGeneration/@Geometry/adddisplacementlayer.m
@@ -0,0 +1,9 @@
+function obj = adddisplacementlayer(obj,displacementLayer)
+%ADDDISPLACEMENTLAYER Summary of this function goes here
+%   Detailed explanation goes here
+
+% validate
+
+obj.displacementLayers(end+1) = displacementLayer;
+end
+
