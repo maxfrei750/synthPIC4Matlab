9d63633af1e926ed767bec46cb4ac7f8b95736cd
Max Frei <max.frei@uni-due.de>
2018-05-15 12:49:51 +0200

    Added input validation.

--- geometryGeneration/@Geometry/adddisplacementlayer.m
+++ geometryGeneration/@Geometry/adddisplacementlayer.m
@@ -2,7 +2,11 @@ function obj = adddisplacementlayer(obj,displacementLayer)
 %ADDDISPLACEMENTLAYER Summary of this function goes here
 %   Detailed explanation goes here
 
-% validate
+% Validate input.
+validateattributes( ...
+    displacementLayer, ...
+    {'Displacement'}, ...
+    {'nonempty','scalar'});
 
 obj.displacementLayers(end+1) = displacementLayer;
 end
