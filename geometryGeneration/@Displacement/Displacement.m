9280e4b6d652e5caf680ccb3c92802f08323e6c2
Max Frei <max.frei@uni-due.de>
2018-05-15 12:24:04 +0200

    Derive class form the Noise3d-class

--- geometryGeneration/@Displacement/Displacement.m
+++ geometryGeneration/@Displacement/Displacement.m
@@ -1,11 +1,8 @@
-classdef Displacement
+classdef Displacement < Noise3d
     %DISPLACEMENT Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
-        type
-        strength
-        scale
     end
     
     methods
@@ -13,7 +10,13 @@ classdef Displacement
             %DISPLACEMENT Construct an instance of this class
             %   Detailed explanation goes here
             
+            obj = obj@Noise3d(type);
+        end
+        
+        function mesh = applyto(obj,mesh)
+            mesh = mesh.displace(obj);
         end
     end
+    
 end
 
