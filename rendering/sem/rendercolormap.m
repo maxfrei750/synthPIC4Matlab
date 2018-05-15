87780e7ca286e6d411742ae1a096b12d4c799835
Max Frei <max.frei@uni-due.de>
2018-05-15 15:59:30 +0200

    Initial commit.

---
+++ rendering/sem/rendercolormap.m
@@ -0,0 +1,39 @@
+function colorMap = rendercolormap(mesh,width,height)
+%RENDERDIFFUSEMAP Summary of this function goes here
+%   Detailed explanation goes here
+
+%% Set parameters.
+baseColor = ones(1,3)*0;
+
+%% Render the geometry.
+% Set figure properties.
+hFigure = figure;
+hFigure.Visible = 'off';
+hFigure.Color = baseColor;
+
+% Draw the current geometry.
+hPatch = mesh.draw;
+
+% Set patch properties.
+hPatch.EdgeColor = 'none';
+
+% Convert figure to image.
+colorMap = figure2image(hFigure,width,height);
+
+% Close figure.
+close(hFigure);
+
+% Remove redundant color channels.
+colorMap = colorMap(:,:,1);
+colorMap = im2double(colorMap);
+
+% Flip diffuse map.
+colorMap = flipud(colorMap);
+
+%% Push data to gpu, if one is available.
+if isgpuavailable
+    colorMap = gpuArray(colorMap);
+end
+
+end
+
