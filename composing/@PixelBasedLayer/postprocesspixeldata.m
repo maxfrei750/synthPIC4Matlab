db7124ba20bf346c4ae5c4f500ba628b7631f4ac
Max Frei <max.frei@uni-due.de>
2018-05-21 11:21:49 +0200

    Added application of strength and brightness.

--- composing/@PixelBasedLayer/postprocesspixeldata.m
+++ composing/@PixelBasedLayer/postprocesspixeldata.m
@@ -2,17 +2,23 @@ function pixelData = postprocesspixeldata(obj,pixelData)
 %POSTPROCESSPIXELDATA Summary of this function goes here
 %   Detailed explanation goes here
 
-% Clip image.
-pixelData = clip(pixelData,[0 1]);
+% Apply strength.
+pixelData = pixelData*obj.strength;
 
-% Apply inversion
-if obj.inverted
-    pixelData = 1-pixelData;
-end
+% Apply brightness.
+pixelData = pixelData+obj.brightness-0.5;
+
+% Clip image.
+pixelData = clip(pixelData,obj.clipping);
 
 % Apply blur.
 if obj.blurStrength > 0
     pixelData = imgaussfilt(pixelData,obj.blurStrength);
+end
+
+% Apply inversion
+if obj.inverted
+    pixelData = 1-pixelData;
 end
 
 end
