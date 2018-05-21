d2b55357c16459a6eb6e1de6efba1b551c595d2e
Max Frei <max.frei@uni-due.de>
2018-05-21 21:07:03 +0200

    Only render map, if it was not rendered before.

--- rendering/@RenderScene/renderbinaryobjectmap.m
+++ rendering/@RenderScene/renderbinaryobjectmap.m
@@ -1,9 +1,19 @@
 function binaryObjectMap = renderbinaryobjectmap(obj)
 %RENDERBINARYOBJECTMAP Summary of this function goes here
-%   Detailed explanation goes here
 
+% If map was already rendered, then return the already rendered map.
+if ~isempty(obj.binaryObjectMap)
+    binaryObjectMap = obj.binaryObjectMap;
+    return
+end
+
+% Retrieve objectMap.
 objectMap = obj.renderobjectmap;
 
+% Binarize objectMap.
 binaryObjectMap = objectMap > 0;
+
+%% Assign the associated ...Map-attribute of the object.
+obj.binaryObjectMap = binaryObjectMap;
 end
 
