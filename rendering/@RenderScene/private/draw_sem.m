8a645a8078d4411c8438a031e4207c21e18b51f2
Max Frei <max.frei@uni-due.de>
2018-05-28 13:22:41 +0200

    Introduce possibility to color faces instead of vertices.

--- rendering/@RenderScene/private/draw_sem.m
+++ rendering/@RenderScene/private/draw_sem.m
@@ -6,9 +6,16 @@ assert(isa(obj,'Mesh'),'Expected input to be an object of class Mesh.');
 
 %% Draw the geometry.
 hPatch = drawMesh(obj);
-
 hPatch.FaceVertexCData = obj.texture;
-set(hPatch,fphong)
+
+% Set properties of the patch object, based on, whether face or point
+% colors where specified.
+if size(obj.texture,1) == obj.nFaces
+    hPatch.FaceColor = 'flat';
+else 
+    hPatch.FaceColor = 'interp';
+    hPatch.FaceLighting = 'phong';
+end
 
 hPatch.LineStyle = 'none';
 daspect([1 1 1])
