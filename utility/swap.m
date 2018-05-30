6fec1955231ee2a90489e75165d2029c4de259a6
Max Frei <max.frei@uni-due.de>
2018-05-30 10:47:58 +0200

    Initial commit.

---
+++ utility/swap.m
@@ -0,0 +1,5 @@
+function [b,a] = swap(a,b)
+% SWAP swaps to variables.
+%   Source: https://de.mathworks.com/matlabcentral/answers/362680-how-to-swap-values-of-two-variables
+
+% This function has no body!
