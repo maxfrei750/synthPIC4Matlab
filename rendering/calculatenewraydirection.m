cc22410ba0d0c521a4f25cb01ee8ec9d790d1f6b
Max Frei <max.frei@uni-due.de>
2018-05-31 23:17:49 +0200

    Vectorized, so that function can process multiple rays at once.

--- rendering/calculatenewraydirection.m
+++ rendering/calculatenewraydirection.m
@@ -1,32 +1,56 @@
-function newRayDirection = calculatenewraydirection(incidentRay,normalVector,indexOfRefraction1,indexOfRefraction2)
+function newRayDirections = calculatenewraydirection(incidentRay,normalVector,indexOfRefraction1,indexOfRefraction2)
 %CALCULATENEWRAYDIRECTION calculates the new direction of a reflected or refracted light ray.
 % Source: https://www.scratchapixel.com/lessons/3d-basic-rendering/introduction-to-shading/reflection-refraction-fresnel
+
+
 
 % Rename variables to keep equations short.
 I = normalizeVector3d(incidentRay);
 N = normalVector;
-etai = indexOfRefraction1;
-etat = indexOfRefraction2;
 
-cosi = clip(dot(I,N),-1,1);
+nRays = size(I,1);
+
+etai = indexOfRefraction1*ones(nRays,1,'gpuArray');
+etat = indexOfRefraction2*ones(nRays,1,'gpuArray');
+
+cosi = clip(dot(I,N,2),-1,1);
 n = N;
 
-if cosi < 0
-    cosi = -cosi;
-else
-    [etai,etat] = swap(etai,etat);
-    n = -N;
-end
+isNegativeCosi = cosi<0;
+isNonNegativeCosi = ~isNegativeCosi;
 
-eta = etai/etat;
-k = 1-eta^2*(1-cosi^2);
+% if cosi >= 0
+%     [etai,etat] = swap(etai,etat);
+[etai(isNonNegativeCosi),etat(isNonNegativeCosi)] = swap(etai(isNonNegativeCosi),etat(isNonNegativeCosi));
+%     n = -N;
+n(isNonNegativeCosi) = -N(isNonNegativeCosi);
+% else
+cosi(isNegativeCosi) = -cosi(isNegativeCosi);
+% end
 
-if k<0
-    % Total internal refraction. Use reflection formular to compute new ray
-    % direction.
-    newRayDirection = I-2*dot(I,N)*N;
-else
-    newRayDirection = eta*I+(eta*cosi-sqrt(k))*n;
-end
+eta = etai./etat;
+k = 1-eta.^2.*(1-cosi.^2);
+
+isNegativeK = k<0;
+isNonNegativeK = ~isNegativeK;
+
+newRayDirections = zeros(nRays,3,'gpuArray');
+
+% if k<0
+%     % Total internal refraction. Use reflection formular to compute new ray
+%     % direction.
+%     newRayDirections = I-2*dot(I,N)*N;
+newRayDirections(isNegativeK,:) = ...
+    I(isNegativeK,:) - ...
+    2*dot(I(isNegativeK,:),N(isNegativeK,:),2).*N(isNegativeK,:);
+% else
+%     newRayDirections = eta*I+(eta*cosi-sqrt(k))*n;
+newRayDirections(isNonNegativeK,:) = ...
+    eta(isNonNegativeK,:).*I(isNonNegativeK,:) + ...
+    (eta(isNonNegativeK,:).*cosi(isNonNegativeK,:) - ...
+    sqrt(k(isNonNegativeK,:))).*n(isNonNegativeK,:);
+% end
+
+newRayDirections = normalizeVector3d(newRayDirections);
 
 end
