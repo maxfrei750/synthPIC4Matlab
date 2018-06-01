68238fc443d14f005e48f5c39d9ce7d051411a74
Max Frei <max.frei@uni-due.de>
2018-06-02 00:21:06 +0200

    Bugfix: Check texture mode based on hPatch.FaceVertexCData instead of obj.texture.

--- geometrygeneration/@Mesh/draw.m
+++ geometrygeneration/@Mesh/draw.m
@@ -30,11 +30,18 @@ hPatch = drawMesh(obj);
 switch lower(coloringMode)
     case 'texture'
         hPatch.FaceVertexCData = obj.texture;
-        set(hPatch,fphong) 
     case 'objectid'
         colormap('parula');
         hPatch.FaceVertexCData = obj.facesObjectIDs;
-        hPatch.FaceColor = 'flat';
+end
+
+% Set properties of the patch object, based on, whether face or point
+% colors where specified.
+if size(hPatch.FaceVertexCData,1) == obj.nFaces
+    hPatch.FaceColor = 'flat';
+else 
+    hPatch.FaceColor = 'interp';
+    hPatch.FaceLighting = 'phong';
 end
 
 if ~islit
