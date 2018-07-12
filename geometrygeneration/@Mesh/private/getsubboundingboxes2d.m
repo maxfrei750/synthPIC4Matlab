850aa50147f0c5d46d2191aec61cf7cfb2546ed4
Max Frei <max.frei@uni-due.de>
2018-07-12 10:31:06 +0200

    Return subboundingboxes2d as cell-array.

--- geometrygeneration/@Mesh/private/getsubboundingboxes2d.m
+++ geometrygeneration/@Mesh/private/getsubboundingboxes2d.m
@@ -2,14 +2,14 @@ function subBoundingBoxes2d = getsubboundingboxes2d(obj)
 %GETSUBBOUNDINGBOXES2D Summary of this function goes here
 %   Detailed explanation goes here
 
-subBoundingBoxes2d = zeros(obj.nObjects,4);
+subBoundingBoxes2d = cell(obj.nObjects,1);
 
 for iSubMesh = 1:obj.nObjects
     % Extract submesh.
     subMesh = obj.extractsubmesh(iSubMesh);
     
     % Get boundingBox of the subMesh.
-    subBoundingBoxes2d(iSubMesh,:) = subMesh.boundingBox2d;
+    subBoundingBoxes2d{iSubMesh,:} = subMesh.boundingBox2d;
 end
 
 end
