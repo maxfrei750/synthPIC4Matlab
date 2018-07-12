d32b0a9505e02bcabb097c66c0101d25e088b275
Max Frei <max.frei@uni-due.de>
2018-07-12 10:36:42 +0200

    Create output folder if it doesn't exist already.

--- composing/@SyntheticImage/save.m
+++ composing/@SyntheticImage/save.m
@@ -2,6 +2,10 @@ function save(obj,path)
 %SAVE Summary of this function goes here
 %   Detailed explanation goes here
 
+[baseDirectory,~,~] = fileparts(path);
+
+createdirectory(baseDirectory);
+
 data = gather(obj.pixelData);
 
 imwrite(data,path);
