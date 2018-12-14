d0acb2aa4b049e978136ded3cc34654d309f761c
Max Frei <max.frei@uni-due.de>
2018-12-14 11:14:13 +0100

    Let the user specify the point, which defines the rotation axis. By default, let this point be the center of mass.

--- geometrygeneration/@Mesh/rotatearoundaxis.m
+++ geometrygeneration/@Mesh/rotatearoundaxis.m
@@ -1,4 +1,9 @@
-function obj = rotatearoundaxis(obj,axisDirection,angleDegree)
+function obj = rotatearoundaxis(obj,axisDirection,angleDegree,point)
+
+% If no point is specified the, use the center of mass as turning point.
+if nargin<4
+    point = obj.centerOfMass;
+end
 
 % Validate inputs
 validateattributes( ...
@@ -11,12 +16,19 @@ validateattributes( ...
     {'numeric'}, ...
     {'real','finite','nonnan','nonsparse','nonempty','scalar'});
 
+validateattributes( ...
+    point, ...
+    {'numeric'}, ...
+    {'real','finite','nonnan','nonsparse','nonempty','row','vector','numel',3});
+
+
+
 % Don't perform a rotation if the rotation angle is zero.
 if angleDegree ~= 0
     % Rotation goes through the center of mass in the
     % specified direction.
     rotationAxis = ...
-        [obj.centerOfMass axisDirection];
+        [point axisDirection];
     
     % Calculate rotation matrix.
     rotationMatrix = createRotation3dLineAngle( ...
