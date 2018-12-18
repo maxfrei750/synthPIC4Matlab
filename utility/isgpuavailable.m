f14b668d8548dd432f727de44e827daf7ef0f22a
Max Frei <max.frei@uni-due.de>
2018-12-18 14:57:48 +0100

    Improved performance.

--- utility/isgpuavailable.m
+++ utility/isgpuavailable.m
@@ -1,20 +1,7 @@
 function tf = isgpuavailable
 %ISGPUAVAILABLE Determines whether a compatible GPU is available.
 %   Returns true, if a compatible GPU is available and false if not.
-%   The actual test is only performed on the first call to
-%   |isgpuavailable|.
 
-persistent isAvailable
-
-if isempty(isAvailable)
-    try
-        gpu = gpuDevice;
-        isAvailable = gpu.SupportsDouble;
-    catch
-        isAvailable = false;
-    end
-end
-
-tf = isAvailable;
+tf = gpuDeviceCount>0;
 
 end
